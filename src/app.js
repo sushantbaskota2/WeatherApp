8445bf98baf37e8efe2bfb652718b563e0aa43ce
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-02-07 02:18:31 -0600

    Added partials handlebars and 404 page

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
 
 const publicDir = path.join(__dirname, '../public');
-const viewPath = path.join(__dirname, '../templates');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
+hbs.registerPartials(partialPath);
 app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'weather app',
-        name: 'sushantey'
+        title: 'Weather App',
+        name: 'Sushant Baskota'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        hamro: 'weather app',
-        tmro: 'sushantey'
+        title: 'Weather App',
+        name: 'Sushant Baskota'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        forecast: 'Khatra jaado xa mug',
-        place: 'Philadelphia'
+        title: 'Weather App',
+        name: 'Sushant Baskota'
     });
 });
 
@@ -36,6 +39,17 @@ app.get('/weather', (req, res) => {
         place: 'Philadelphia'
     });
 });
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        errorMessage: 'Help article not found'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404page', { errorMessage: 'Page not found' });
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
